9305776a53f38af4931094bc5a67c0f5c3b18c15
bambache@gmail.com <ebambach@users.noreply.github.com>
2017-09-08 13:36:30 -0400

    sourcetree

---
+++ StudentClassProject/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentClassProject {
+	class Program {
+		static void Main(string[] args) {
+			new Program().Run();
+		}
+		void Run() {
+			//Student Class variables
+			//string Name;
+			//string EyeColor;
+			//DateTime DateRegistered;
+			//bool IsLikesMusic;
+			//int YearsExperience;
+			Student sa = new Student("Sue Ann", "Hazel");
+			sa.SetYearsExperience(0);
+			Student eb = new Student("Eric B");
+			sa.SetEyeColor();
+			sa.SetYearsExperience(2);
+			Student be = new Student("Bob E", "Blue");
+			//Because we made a constructor that is passed two strings, Name and EyeColor,
+			//we can boil the two separate sa.SetName() and sa.SetEyeColor() methods into the
+			//singular Student go = new Student("Greg O", "Blue")
+			Student go = new Student("Greg O", "Blue");
+			Student es = new Student("Eric S", "Blue");
+			sa.SetYearsExperience(3);
+		}
+	}
+}
---
+++ StudentClassProject/Student.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentClassProject {
+	class Student {
+		string Name;
+		string EyeColor;
+		DateTime DateRegistered = DateTime.Now;
+		bool IsLikesMusic = true;
+		int YearsExperience = 1;
+
+		public void SetDateRegistered(DateTime DR) {
+			DateRegistered = DR;
+		}
+		public void SetEyeColor(string EC = "Brown") {
+			EyeColor = EC;
+		}
+		public void SetIsLikesMusic(bool ILM = true) {
+			IsLikesMusic = ILM;
+		}
+		public void SetName(string name) {
+			Name = name;
+		}
+		public void SetYearsExperience(int YE) {
+			YearsExperience = YE;
+		}
+		//Constructors
+		public Student() {
+
+		}
+		public Student(string name) {
+
+		}
+		public Student(string name, string eyecolor) {
+
+		}
+		public Student(string name, string EC, DateTime DR) {
+
+		}
+		public Student(string name, string EC, DateTime DR, bool ILM) {
+
+		}
+		public Student(string name, string EC, DateTime DR, bool ILM, int YE) {
+
+		}
+	}
+	}
